f8c7d51ae061831252e02321583af98f319094bb
tsutanosa <atarou.shu.ko@gmail.com>
2025-02-18 22:15:53 +0900

    得点加算その1

---
+++ 2025winterGamejam/Assets/Scripts/Domain/UseCase/InGame/Player/AddPointCase.cs
@@ -0,0 +1,39 @@
+using System;
+using Domain.IModel.InGame;
+using Structure.InGame;
+using Domain.IModel.InGame.Player;
+using R3;
+namespace Domain.UseCase.InGame.Player
+{
+    public class AddPointCase:IDisposable
+    {
+        public AddPointCase
+        (
+            IScoreModelPlayer scoreModelPlayerModel,
+            IPlayerIdModel playerIdModel,
+            IJudgeEventModel judgeEventModel
+        )
+        {
+            ScoreModelPlayer = scoreModelPlayerModel;
+            PlayerIdModel = playerIdModel;
+            JudgeEventModel = judgeEventModel;
+
+            JudgeEventModel.JudgeEndEvent += AddPoint;
+        }
+
+        private void AddPoint(BattleResult battleResult)
+        {
+            
+        }
+
+        private IScoreModelPlayer ScoreModelPlayer{get;}
+        private IPlayerIdModel PlayerIdModel{get;}
+        private IJudgeEventModel JudgeEventModel{get;}
+        
+        public void Dispose()
+        {
+            JudgeEventModel.JudgeEndEvent -= AddPoint;
+        }
+    }
+
+}
