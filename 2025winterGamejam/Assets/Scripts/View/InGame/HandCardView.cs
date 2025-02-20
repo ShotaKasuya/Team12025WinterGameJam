5649502526d71d0ae5e85c51b93d24a63f846d2c
manato <23rd177@ms.dendai.ac.jp>
2025-02-20 14:41:27 +0900

    selectionevent呼び出し

--- 2025winterGamejam/Assets/Scripts/View/InGame/HandCardView.cs
+++ 2025winterGamejam/Assets/Scripts/View/InGame/HandCardView.cs
@@ -1,8 +1,14 @@
 using IView.InGame;
+using log4net.Filter;
+using UnityEngine;
 
 namespace View.InGame
 {
     public class HandCardView : FactorableCardView
     {
+        void OnMouseDown()
+        {
+            SelectionEvent.Invoke(Card);
+        }
     }
 }
