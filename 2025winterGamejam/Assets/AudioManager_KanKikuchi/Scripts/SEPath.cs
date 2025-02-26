0bd1c80ac2e6ff95ca6422e41a077445990138b2
koma3 <limit307703>
2025-02-26 20:06:30 +0900

    タイトル実装(シーン遷移未実装)+AudioManagerインポート

---
+++ 2025winterGamejam/Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -0,0 +1,16 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// SEファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class SEPath{
+
+	public const string DECISION_SE = "SE/DecisionSE";
+	public const string JINGLE10    = "SE/Jingle/jingle10";
+	public const string SELECTED_SE = "SE/SelectedSE";
+	public const string SYSTEM20    = "SE/system20";
+	public const string SYSTEM23    = "SE/system23";
+
+}
+
+}
---
+++ 2025winterGamejam/Assets/UI/UIScripts/ButtonIC.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using KanKikuchi.AudioManager;
+using UnityEngine.SceneManagement;
+
+public class ButtonIC : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite highlightedSprite; // カーソルが重なったときの画像
+    private Image buttonImage;
+    private Sprite normalSprite; // ボタンの通常の画像
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>(); // 修正: 型を明示
+        normalSprite = buttonImage.sprite; // ボタンの通常の画像を記憶する
+
+        // ボタンコンポーネントがあればクリックイベントを追加
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(OnButtonClick);
+        }
+    }
+
+    // カーソルがボタンに重なったときの処理
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = highlightedSprite; // カーソルが重なったときの画像に変更する
+        SEManager.Instance.Play(SEPath.SELECTED_SE, 0.3f);
+    }
+
+    // カーソルがボタンから離れたときの処理
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.sprite = normalSprite; // 通常の画像に戻す
+    }
+
+    // ボタンがクリックされたときの処理
+    public void OnButtonClick()
+    {
+        SEManager.Instance.Play(SEPath.DECISION_SE, 0.3f);
+    }
+
+}
