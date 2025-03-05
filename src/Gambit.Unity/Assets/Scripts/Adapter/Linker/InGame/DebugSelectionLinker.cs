44f60bf61986e70a79d4998a1a07822f95324ae6
manato <23rd177@ms.dendai.ac.jp>
2025-03-05 17:05:14 +0900

    DebugSelectionLinker

---
+++ src/Gambit.Unity/Assets/Scripts/Adapter/Linker/InGame/DebugSelectionLinker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Adapter.IModel.InGame.Judgement;
+using Adapter.IModel.InGame.Player;
+using Adapter.IView.InGame;
+using Utility.Module.Option;
+using Utility.Structure.InGame;
+using VContainer.Unity;
+
+namespace Adapter.Linker.InGame
+{
+    public class DebugSelectionLinker : IInitializable, IDisposable
+    {
+        public DebugSelectionLinker
+        (
+            IHandCardPoolView handCardPoolView,
+            IMutSelectedCardModel selectedCardModel
+        )
+        {
+            HandCardPoolView = handCardPoolView;
+            SelectedCardModel = selectedCardModel;
+        }
+
+        public void Initialize()
+        {
+            HandCardPoolView.OnStore += SetLinker;
+            HandCardPoolView.OnPop += RemoveLinker;
+        }
+
+        private void SetLinker(ProductCardView view)
+        {
+            view.SelectionEvent += OnSelect;
+        }
+
+        private void RemoveLinker(ProductCardView view)
+        {
+            view.SelectionEvent -= OnSelect;
+        }
+
+        private void OnSelect(PlayerCard selectedCard)
+        {
+            var currentSelect = SelectedCardModel.GetSelection(selectedCard.PlayerId);
+            var apply = Option<PlayerCard>.Some(selectedCard);
+
+            if (currentSelect.TryGetValue(out var card))
+            {
+                if (card.Card == selectedCard.Card)
+                {
+                    apply = Option<PlayerCard>.None();
+                }
+            }
+
+            SelectedCardModel.StorePlayerSelection(selectedCard.PlayerId.Id, apply);
+            ApplyView(selectedCard.PlayerId, apply);
+        }
+
+        private void ApplyView(PlayerId playerId, Option<PlayerCard> selected)
+        {
+            var views = HandCardPoolView.GetViewList(playerId);
+            
+            foreach (var cardView in views)
+            {
+                cardView.TurnOff();
+            }
+
+            if (selected.TryGetValue(out var card))
+            {
+                foreach (var cardView in views)
+                {
+                    if (cardView.Card.Card == card.Card)
+                    {
+                        cardView.TurnOn();
+                    }
+                }
+            }
+        }
+
+        private IHandCardPoolView HandCardPoolView { get; }
+        private IMutSelectedCardModel SelectedCardModel { get; }
+
+        public void Dispose()
+        {
+            HandCardPoolView.OnPop -= SetLinker;
+            HandCardPoolView.OnStore -= RemoveLinker;
+        }
+    }
+}
