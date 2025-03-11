17897926aeb20bb2c4b96392c286a50dd1e2264a
koma3 <limit307703>
2025-03-11 15:25:09 +0900

    対戦画面UI仮配置

--- src/Gambit.Unity/Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ src/Gambit.Unity/Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -6,9 +6,12 @@
 public static class BGMPath{
 
 	public const string BATTLE27    = "BGM/Battle/battle27";
+	public const string BATTLE_BGM  = "BGM/BattleBGM";
 	public const string FANTASY14   = "BGM/fantasy14";
 	public const string HEARTBEAT01 = "BGM/heartbeat01";
+	public const string LOSE_BGM    = "BGM/LoseBGM";
 	public const string TITLE_BGM   = "BGM/TitleBGM";
+	public const string WIN_BGM     = "BGM/WinBGM";
 
 }
 
