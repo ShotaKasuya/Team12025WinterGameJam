a59027a6766c42669658c4d1f494d0e334cc9720
manato <23rd177@ms.dendai.ac.jp>
2025-03-11 15:06:34 +0900

    GameResultView修正

--- src/Gambit.Unity/Assets/Scripts/Adapter/View/InGame/Ui/GameResultView.cs
+++ src/Gambit.Unity/Assets/Scripts/Adapter/View/InGame/Ui/GameResultView.cs
@@ -5,8 +5,30 @@ namespace Adapter.View.InGame.Ui
 {
     public class GameResultView : MonoBehaviour, IGameResultView
     {
+        [SerializeField] SpriteRenderer spriteRenderer;
+        [SerializeField] private Sprite Win;
+        [SerializeField] private Sprite lose;
+        [SerializeField] private Sprite draw;
+
+        public GameResultView()
+        {
+            spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        }
+        
         public void Enable(IGameResultView.Result result)
         {
+            switch (result)
+            {
+                case IGameResultView.Result.Win:
+                    spriteRenderer.sprite = Win;
+                    break;
+                case IGameResultView.Result.Lose:
+                    spriteRenderer.sprite = lose;
+                    break;
+                case IGameResultView.Result.Draw:
+                    spriteRenderer.sprite = draw;
+                    break;
+            }
             gameObject.SetActive(true);
         }
         
